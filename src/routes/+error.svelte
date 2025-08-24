44a1eebf9e184a12829ff3b2f9378be6c1c70dfb
J4y_boi <57105859+j4y-boi@users.noreply.github.com>
2025-08-24 21:40:22 +0200

    added 500 error page in case

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,12 +2,18 @@
     import TyperwriterText from "$lib/TyperwriterText.svelte";
     import Typewriter from "$lib/Typewriter.svelte";
     import Bottomfooter from "$lib/bottomfooter.svelte";
+    import { page } from '$app/state';
 </script>
 
 <div class="min-h-screen flex flex-col">
     <div class="text-white center">
-        <Typewriter speedmin={350} speedmax={350} text="404"/>
-        <TyperwriterText speedmin={30} speedmax={30} text="We couldn't find that page!"/><br>
+        {#if page.status === 404}
+            <Typewriter speedmin={350} speedmax={350} text="404"/>
+            <TyperwriterText speedmin={30} speedmax={30} text="We couldn't find that page!"/><br>
+        {:else if page.status === 500}
+            <Typewriter speedmin={350} speedmax={350} text="500"/>
+            <TyperwriterText speedmin={30} speedmax={30} text="Something went wrong on our end..."/><br>
+        {/if}
         <div class="flex justify-center p-8 center">
             <button class="bg-white  text-black font-bold p-4 rounded transition active:scale-95 hover:scale-105" style="margin-right: 10px">
                 <a href="/">Return home </a>
